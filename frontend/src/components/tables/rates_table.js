50cc4ebdeb27e7b81f1a9a5578dc6388dc849bf0
charlie0054a <charliefernandis28@gmail.com>
2024-06-12 12:43:28 +0530

    sunag

--- frontend/src/components/tables/rates_table.js
+++ frontend/src/components/tables/rates_table.js
@@ -38,7 +38,7 @@ const Rates_table = ({ productID }) => {
 
       <div className='rate-table-header'>
         <div>Party Name</div>
-        <div>Invoice Number</div>
+        <div>Purchase Number</div>
         <div>MRP</div>
         <div>Discount</div>
         <div>Deal</div>
@@ -58,7 +58,7 @@ const Rates_table = ({ productID }) => {
               >
                 <div className='rate-table-row'>
                   <div className='rate-table-column'>{rate.partyID.partyName.length > 7 ? rate.partyID.partyName.slice(0, 9) : rate.partyID.partyName}</div>
-                  <div className='rate-table-column'>{rate.purchase.invoiceNo.length > 8 ? rate.purchase.invoiceNo.slice(2, rate.purchase.invoiceNo.length) : rate.purchase.invoiceNo}</div>
+                  <div className='rate-table-column'>{rate.purchase.billNo}</div>
                   <div className='rate-table-column'>{rate.mrp}</div>
                   <div className='rate-table-column'>{rate.discount}</div>
                   <div className='rate-table-column'>{0}</div>
@@ -80,24 +80,13 @@ const Rates_table = ({ productID }) => {
                   flexDirection: "column",
                   padding: "10px 50px",
                   boxSizing: "border-box"
-                  // color:"white",
-                  // fontWeight:"bold"
                 }}>
-                {/* <div>
-                  <div className='accordian-hidden-box'><div>Bill Number:</div> <div>{rate.purchase.billNo}</div></div>
-                  <div className='accordian-hidden-box'>Bill Date: &nbsp;&nbsp;{rate.purchase.billDate}</div>
-                  <div className='accordian-hidden-box'>Full Invoice Number: &nbsp;&nbsp;{rate.purchase.invoiceNo}</div>
-                  <div className='accordian-hidden-box'>Batch Number: &nbsp;&nbsp;{rate.batchNumber}</div>
-                  <div className='accordian-hidden-box'>Quantity: &nbsp;&nbsp;{rate.quantity}</div>
-                  <div className='accordian-hidden-box'>Free:&nbsp;&nbsp; {rate.free}</div>
-                </div> */}
                 <div>
-                  <div className='accordian-hidden-box'><div>Bill Number:</div> <div>{rate.purchase.billNo}</div></div>
-                  <div className='accordian-hidden-box'><div>Bill Date:</div> <div>{rate.purchase.billDate}</div></div>
-                  <div className='accordian-hidden-box'><div>Full Invoice Number:</div> <div>{rate.purchase.invoiceNo}</div></div>
-                  <div className='accordian-hidden-box'><div>Batch Number:</div> <div>{rate.batchNumber}</div></div>
-                  <div className='accordian-hidden-box'><div>Quantity:</div> <div>{rate.quantity}</div></div>
-                  <div className='accordian-hidden-box'><div>Free:</div> <div>{rate.free}</div></div>
+                  <div className='accordian-hidden-box'><div style={{fontWeight:"bold"}}>Invoice Number:</div> <div>{rate.purchase.invoiceNo}</div></div>
+                  <div className='accordian-hidden-box'><div style={{fontWeight:"bold"}}>Bill Date:</div> <div>{rate.purchase.billDate}</div></div>
+                  <div className='accordian-hidden-box'><div style={{fontWeight:"bold"}}>Batch Number:</div> <div>{rate.batchNumber}</div></div>
+                  <div className='accordian-hidden-box'><div style={{fontWeight:"bold"}}>Quantity:</div> <div>{rate.quantity}</div></div>
+                  <div className='accordian-hidden-box'><div style={{fontWeight:"bold"}}>Free:</div> <div>{rate.free}</div></div>
                 </div>
               </AccordionDetails>
             </Accordion>
