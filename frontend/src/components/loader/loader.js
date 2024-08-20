3ccc2cc53027fb12d9fb6b91f0f188a34f5765b8
charlie0054a <charliefernandis28@gmail.com>
2024-08-20 17:17:55 +0530

    loader improves

--- frontend/src/components/loader/loader.js
+++ frontend/src/components/loader/loader.js
@@ -1,10 +1,25 @@
 // Loader.js
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './loader.css'; // Import the CSS file for styling
 
-const Loader = () => {
+const Loader = ({initialTime = 0}) => {
+
+  const [time, setTime] = useState(initialTime);
+
+
+  useEffect(() => {
+    // Start a timer that increments the time every second
+    const interval = setInterval(() => {
+      setTime((prevTime) => prevTime + 1);
+    }, 1000);
+
+    // Cleanup function to clear the interval when the component unmounts
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="loader-container">
+      <span>{time}</span>
       <div className="loader"></div>
     </div>
   );
